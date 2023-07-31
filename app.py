41c91adce757955d6765eb04b7a4a0f8526a7570
Nerolation <toni_w@gmx.at>
2023-07-31 14:24:38 +0200

    Initial commit

--- app.py
+++ app.py
@@ -8,58 +8,96 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 import dash_bootstrap_components as dbc
+from dash import Input, Output
 
 
-df = pd.read_csv("reorg-data.csv")
-df_table = df.rename(columns={"slot": "Slot", "cl_client": "CL Client", "validator_id": "Val. ID", "date": "Date", "slot_in_epoch": "Slot Nr. in Epoch"})
-df_table.sort_values("Slot", ascending=False, inplace=True)
+# Data preparation
+def prepare_data():
+    df = pd.read_csv("reorg-data.csv")
+    df_table = df.rename(columns={"slot": "Slot", "cl_client": "CL Client", "validator_id": "Val. ID", "date": "Date", "slot_in_epoch": "Slot Nr. in Epoch"})
+    df_table.sort_values("Slot", ascending=False, inplace=True)
 
+    df_per_sie = df['slot_in_epoch'].value_counts().reset_index()
+    df_per_sie.set_index('slot_in_epoch', inplace=True)
+    df_per_sie = df_per_sie.reindex(range(0, 32))
+    df_per_sie.fillna(0, inplace=True)
+    df_per_sie.reset_index(inplace=True)
+    df_per_sie.rename(columns={'index': 'slot_in_epoch'}, inplace=True)
 
-df_per_sie = df['slot_in_epoch'].value_counts().reset_index()
-df_per_sie.set_index('slot_in_epoch', inplace=True)
-df_per_sie = df_per_sie.reindex(range(0, 32))
-df_per_sie.fillna(0, inplace=True)
-df_per_sie.reset_index(inplace=True)
-df_per_sie.rename(columns={'index': 'slot_in_epoch'}, inplace=True)
+    return df, df_table, df_per_sie
 
-# Create a figure using Plotly Express
-fig = px.histogram(df, x='date', title='Number of Missed Blocks Over Date', nbins=100)
-fig2 = px.bar(df['cl_client'].value_counts().reset_index(), x='count', y='cl_client', title='Number of Missed Slots per Client', labels={'cl_client': 'Client', 'cl_client': 'Count'})
-fig3 = go.Figure(data=go.Bar(x=df_per_sie['slot_in_epoch'], y=df_per_sie['count']))
+# Figures
+def create_figures(df, df_per_sie):
+    fig1 = px.histogram(df, x='date', nbins=100)
+    fig1.update_layout(
+        title='Number of Missed Blocks Over Date',
+        xaxis_title='Date',
+        margin=dict(l=20, r=20, t=40, b=20)
+    )
+    fig1.update_yaxes(title_standoff=5)
 
-# Set title and labels
-fig3.update_layout(
-    title='Number of Missed Slots per Slot Nr. in Epoch',
-    xaxis_title='Slot in Epoch',
-    yaxis_title='Count'
-)
+    fig2 = px.bar(df['cl_client'].value_counts().reset_index(), x='count', y='cl_client', labels={'cl_client': 'Client', 'count': ''})
+    fig2.update_layout(
+        title='Number of Missed Slots per Client',
+        xaxis_title='Slot in Epoch',
+        margin=dict(l=20, r=20, t=40, b=20)
+    )
+    fig2.update_yaxes(title_standoff=5)
 
-# Set x-axis ticks
-fig3.update_xaxes(tickvals=list(range(32)))
+    fig3 = go.Figure(data=go.Bar(x=df_per_sie['slot_in_epoch'], y=df_per_sie['count']))
+    fig3.update_layout(
+        title='Number of Missed Slots per Slot Nr. in Epoch',
+        xaxis_title='Slot in Epoch',
+        yaxis_title='',
+        margin=dict(l=20, r=20, t=40, b=20)
+    )
+    fig3.update_yaxes(title_standoff=5)
+    fig3.update_xaxes(tickvals=list(range(32)))
+
+    return fig1, fig2, fig3
+
+df, df_table, df_per_sie = prepare_data()
+fig1, fig2, fig3 = create_figures(df, df_per_sie)
+
+# Layout based on window size
+def get_layout(width):
+    if width <= 800:
+        font_size = 8
+    else:
+        font_size = 20
+
+    return dict(
+        title='Number of Missed Slots per Slot Nr. in Epoch',
+        xaxis_title='Slot in Epoch',
+        yaxis_title='',
+        margin=dict(l=20, r=20, t=40, b=20),
+        font={'size': font_size}
+    )
+
 # Initialize the Dash app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.scripts.append_script({"external_url": "update_window_width.js"})
+app.clientside_callback(
+    "window.dash_clientside.update_window_size",
+    Output('window-size-store', 'data'),
+    Input('window-size-trigger', 'n_intervals')
+)
 server = app.server
-#app = JupyterDash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-def update_styles(selected_cells, data):
-    if selected_cells:
-        row = selected_cells[0]['row']
-        col = selected_cells[0]['column_id']
-        if col == 'slot':
-            slot = data[row]['slot']
-            link = f"https://beaconcha.in/slot/{slot}"
-            webbrowser.open(link, new=2)
-            return []
-    return []
+
 
 app.layout = dbc.Container(
     [
+        # Title
         dbc.Row(html.H1("Reorg Dashboard", style={'text-align': 'center'}), className="mb-4"),
+
+        # Author
         dbc.Row(
             html.H5(
-                ['Built with 🖤 by ',
-                 html.A('Toni Wahrstätter', href='https://twitter.com/nero_eth', target='_blank')]
+                ['Built with 🖤 by ', html.A('Toni Wahrstätter', href='https://twitter.com/nero_eth', target='_blank')]
             ), className="mb-4"),
+
+        # DataTable
         dbc.Row(
             dbc.Col(
                 dash_table.DataTable(
@@ -71,7 +109,11 @@ app.layout = dbc.Container(
                         {'if': {'column_id': 'CL Client'}, 'maxWidth': '80px'}
                     ],
                     id='table',
-                    columns=[{"name": i, "id": i, 'presentation': 'markdown'} if i == 'Slot' else {"name": i, "id": i} for i in df_table.columns],
+                    columns=[
+                        {"name": i, 
+                         "id": i, 
+                         'presentation': 'markdown'} if i == 'Slot' else {"name": i, "id": i} for i in df_table.columns
+                    ],
                     data=df_table.to_dict('records'),
                     page_size=50,
                     style_table={'overflowX': 'auto'},
@@ -83,22 +125,40 @@ app.layout = dbc.Container(
                         {'if': {'column_id': 'Slot Nr. in Epoch'}, 'text-align': 'center'},
                     ],
                     css=[dict(selector="p", rule="margin: 0; text-align: center")],
-
                 ),
-                className="mb-4", # margin bottom
-                md=12 # Full width on medium and larger screens
-
+                className="mb-4", md=12
             )
         ),
-        dbc.Row(dbc.Col(dcc.Graph(id='graph1', figure=fig), md=12, className="mb-4")), # Full width on medium and larger screens, margin bottom
-        dbc.Row(dbc.Col(dcc.Graph(id='graph2', figure=fig2), md=12, className="mb-4")), # Full width on medium and larger screens, margin bottom
-        dbc.Row(dbc.Col(dcc.Graph(id='graph3', figure=fig3), md=12, className="mb-4")), # Full width on medium and larger screens, margin bottom
+
+        # Graphs
+        dbc.Row(dbc.Col(dcc.Graph(id='graph1', figure=fig1), md=12, className="mb-4")),
+        dbc.Row(dbc.Col(dcc.Graph(id='graph2', figure=fig2), md=12, className="mb-4")),
+        dbc.Row(dbc.Col(dcc.Graph(id='graph3', figure=fig3), md=12, className="mb-4")),
+
+        # Additional Components
+        dbc.Row(dcc.Interval(id='window-size-trigger', interval=1000, n_intervals=0)),
+        dcc.Store(id='window-size-store')
     ],
-    fluid=True, # Full-width layout
+    fluid=True,
 )
+
+
+# Callbacks
+@app.callback(
+    Output('graph3', 'figure'),
+    Input('window-size-store', 'data')
+)
+def update_layout(window_size_data):
+    if window_size_data is None:
+        raise dash.exceptions.PreventUpdate
+
+    width = window_size_data['width']
+    fig3.update_layout(**get_layout(width))
+    return fig3
 
 
 if __name__ == '__main__':
     #app.run_server(debug=True)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    
